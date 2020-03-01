d71fe3055f8d0744c966eb72b0c8cd827e42eb4b
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-03-01 17:04:16 +0000

    Git history nuked

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript Utilities by Neruthes
+
+## Readability
+
+```javascript
+javascript:(function(u){var xhr=new XMLHttpRequest();xhr.open('GET','//neruthes.xyz/jsu/'+u);xhr.onload=function(){eval(xhr.responseText);};xhr.send();console.log('Loaded '+u);})('readability/type-strong-sansserif.js')
+```
---
+++ readability/type-strong-serif.js
@@ -0,0 +1,51 @@
+/*
+	@author Neruthes
+	@year 2020
+	@license AGPL-3.0
+*/
+
+(function(styleTag){
+	var styleText = `html, body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, aside, textarea, input, button, blockquote, form, table, tbody, tl, td, tr, article, section, div, a, figure, header, footer, q, cite, em, strong, address, time, dfn, caption
+	{
+		font-family: "Source Serif Pro", "Charter", "Iowan Old Style", "Georgia", "Merriweather", "Times New Roman", "Times", serif, "Noto Serif SC", "Source Han Serif CN", "Stongti SC" !important;
+	}
+	html, body, p, ul, ol, li, aside, textarea, input, button, blockquote, form, table, tbody, tl, td, tr, article, section {
+		font-size: 20px !important;
+		font-weight: 400 !important;
+	}
+	pre, pre *, code, code * {
+		font-family: "JetBrains Mono", "Source Code Pro", "Inconsolata", "Menlo", Consolas, monospace !important;
+	}`;
+	if (styleTag) {
+		styleTag.innerHTML = styleText;
+	} else {
+		var st = document.createElement('style');
+		st.setAttribute('id', 'neruthes-typeface-replacement-text');
+		st.innerHTML = styleText;
+		document.head.appendChild(st);
+	};
+	var styleDict = {
+		fontSize: {
+			h1: 40,
+			h2: 36,
+			h3: 30,
+			h4: 28,
+			h5: 26,
+			h6: 22,
+			p: 20,
+		},
+		fontWeight: {
+			h1: 600,
+			h2: 600,
+			h3: 600,
+			h4: 600,
+			h5: 600,
+			h6: 600,
+			p: 400,
+		}
+	};
+	document.querySelectorAll('h1, h2, h3, h4, h5, h6, p').forEach(node => {
+		node.style.fontSize = (styleDict.fontSize)[node.tagName.toLowerCase()] + 'px';
+		node.style.fontSize = (styleDict.fontWeight)[node.tagName.toLowerCase()] + '';
+	});
+})(document.getElementById('neruthes-typeface-replacement-text'));
