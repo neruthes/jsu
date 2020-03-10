aa1de9b45699dae404b1b6c21895e526703f8fd9
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-03-10 19:25:15 +0000

    Regular update: 2020-03-10 19:25:15 UTC

---
+++ readability/wikipedia.m.js
@@ -0,0 +1,35 @@
+/*
+	@author Neruthes
+	@year 2020
+	@license AGPL-3.0 https://www.gnu.org/licenses/agpl-3.0.html
+*/
+
+(function(styleTag){
+	var styleText = `
+	@import url(https://neruthes.xyz/fonts/jetbrains-mono.css);
+	html, body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, aside, textarea, input, button, blockquote, form, table, tbody, tl, td, tr, article, section, div, a, figure, header, footer, q, cite, em, strong, address, time, dfn, caption
+	{
+	}
+	html, body, p, ul, ol, li, aside, textarea, input, button, blockquote, form, table, tbody, tl, td, tr, article, section {
+		font-size: 22px !important;
+		font-weight: 400 !important;
+	}
+	#content {
+		border: none;
+	}
+	body,
+	#mw-page-base {
+		background: #FFF;
+	}
+	pre, pre *, code, code * {
+		font-family: "JetBrains Mono", "Source Code Pro", "Inconsolata", "Menlo", Consolas, monospace !important;
+	}`;
+	if (styleTag) {
+		styleTag.remove();
+	} else {
+		var st = document.createElement('style');
+		st.setAttribute('id', 'neruthes-xyz--jsu-readability--wikipedia-m');
+		st.innerHTML = styleText;
+		document.head.appendChild(st);
+	};
+})(document.getElementById('neruthes-xyz--jsu-readability--wikipedia-m'));
