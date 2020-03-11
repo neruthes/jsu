95888a81e8d84ec54a22dc7ea456007523e334f0
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-03-11 17:58:04 +0000

    Regular update: 2020-03-11 17:58:04 UTC

--- readability/switch-font.js
+++ readability/switch-font.js
@@ -13,9 +13,9 @@
 	}
 */
 window.uuid_9a5a3ad359c2416ead609c80962d03e6_func = function (argv, callback) {
-	var givenState = 1;
-	if (argv) {
-		givenState = argv.font || givenState;
+	var givenState = argv ? argv.font : undefined;
+	if ([0,1,2,3,undefined].indexOf(givenState)) {
+		givenState = undefined;
 	};
 	var txt = function (st) {
 		if (st === 0) {
@@ -24,6 +24,7 @@ window.uuid_9a5a3ad359c2416ead609c80962d03e6_func = function (argv, callback) {
 		return `html, body, h1, h2, h3, h4, h5, h6, p, ul, ol, li, aside, textarea, input, button, blockquote, form, table, tbody, tl, td, tr, article, section, div, a, figure, header, footer, q, cite, em, strong, address, time, dfn, caption
 		{
 			${[
+				'',
 				'font-family: -apple-system, "SF Pro Text", "SF Pro UI", "SF UI Text", "Helvetica Neue", "Helvetica", "Segoe UI", "Roboto", "Product Sans", "Droid Sans", "Arial", sans-serif, "PingFang SC", "Noto Sans CJK SC", "Source Han Sans CN", "Hiragino Sans GB", "Hiragino Sans", "Heiti SC", "Microsoft YaHei" !important;',
 				'font-family: "Source Serif Pro", "Charter", "Iowan Old Style", "Georgia", "Merriweather", "Times New Roman", "Times", serif, "Noto Serif SC", "Source Han Serif CN", "Stongti SC" !important;',
 				'font-family: "JetBrains Mono", "Source Code Pro", "Inconsolata", "Menlo", Consolas, monospace !important;'
@@ -53,7 +54,7 @@ window.uuid_9a5a3ad359c2416ead609c80962d03e6_func = function (argv, callback) {
 	var styleTag = document.getElementById('uuid_9a5a3ad359c2416ead609c80962d03e6');
 
 	if (styleTag) {
-		var newState = givenState || (parseInt(styleTag.getAttribute('data-state')) + 1) % 4;
+		var newState = (givenState !== undefined) ? givenState : (parseInt(styleTag.getAttribute('data-state')) + 1) % 4;
 		styleTag.innerHTML = txt(newState);
 		styleTag.setAttribute('data-state', newState);
 	} else {
